1a2b4be91a95eb2300b2b1ede3a9700809045c95
Thierry Treyer <zehkae@gmail.com>
2017-02-22 15:25:40 +0100

    Convert Float32 back to Float

--- src/main/scala/Float32.scala
+++ src/main/scala/Float32.scala
@@ -5,11 +5,21 @@ object Float32 {
   def floatToFloat32(x: Float): Float32 = {
     val bits: Int = Float.floatToRawIntBits(x)
 
-    val sign:     Int = (bits & 0x80000000) >>> 31;
-    val exponent: Int = (bits & 0x7f800000) >>> 23;
-    val mantissa: Int = (bits & 0x007fffff) >>>  0;
+    val sign:     Int = (bits & 0x80000000) >>> 31
+    val exponent: Int = (bits & 0x7f800000) >>> 23
+    val mantissa: Int = (bits & 0x007fffff) >>>  0
 
     new Float32(sign, exponent, mantissa)
+  }
+
+  def float32ToFloat(f: Float32): Float = {
+    val sign:     Int = f.sign     <<< 31
+    val exponent: Int = f.exponent <<< 23
+    val mantissa: Int = f.mantissa <<<  0
+
+    val bits: Int = sign | exponent | mantissa
+
+    Float.intBitsToFloat(bits)
   }
 
 }
