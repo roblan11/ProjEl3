eec3cb69953e61464ca467eb9f0e4ac8243e9f68
roblan11 <roblan11@hotmail.ch>
2017-02-25 21:16:16 +0100

    Division finished, small improvements to multiplication

--- src/main/scala/Float32.scala
+++ src/main/scala/Float32.scala
@@ -31,16 +31,18 @@ class Float32(val sign: Int, val exponent: Int, val mantissa: Int) {
   }
 
   def *(that: Float32): Float32 = {
-    var mantissa  = this.mantissa + that.mantissa
-    var blocker   = 0x00400000
+    val sign     :Int = this.sign ^ that.sign
+    var exponent :Int = this.exponent + that.exponent
+    var mantissa :Int = this.mantissa + that.mantissa
+
+    var blocker :Int = 0x00400000
+
     for(i <- 1 to 23) {
       if( (this.mantissa & blocker) != 0 ) {
         mantissa += (that.mantissa >>> i )
       }
       blocker = blocker >>> 1
     }
-    val sign      = this.sign ^ that.sign
-    var exponent  = this.exponent + that.exponent
 
     if( (mantissa & 0x00800000) != 0) {
       exponent += 1
@@ -48,6 +50,30 @@ class Float32(val sign: Int, val exponent: Int, val mantissa: Int) {
     }
 
     new Float32(sign, exponent, mantissa)
+  }
+
+  def /(that: Float32): Float32 = {
+    val sign     :Int = this.sign ^ that.sign
+    var exponent :Int = this.exponent - that.exponent
+    var mantissa :Int = 0
+
+    var mantissaLeft  :Int = this.mantissa + 0x00800000
+    var mantissaRight :Int = that.mantissa + 0x00800000
+
+    for(i <- 0 to 23) {
+      if( mantissaLeft >= mantissaRight ) {
+        mantissa += (1 << (23 - i))
+        mantissaLeft -= mantissaRight
+      }
+      mantissaRight = mantissaRight >>> 1
+    }
+
+    if( (mantissa & 0x00800000) == 0) {
+      exponent -= 1
+      mantissa = mantissa << 1
+    }
+
+    new Float32(sign, exponent, mantissa & 0x007fffff)
   }
 
 }
